8acb85812c6762645d1891976fde29291e158bef
SamikHub <sam.dovhyy@icloud.com>
2024-12-09 13:49:06 +0200

    0.0.0

--- app/models.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-class License(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(80), unique=True, nullable=False)
-    valid = db.Column(db.Boolean, default=True)
